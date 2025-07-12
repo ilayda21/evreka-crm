af3cbe16cc280e02050a9ba6c6971c635411f6ce
Ilayda Cavusoglu Pars <cavusogluilayda@gmail.com>
2025-07-12 23:04:30 +0300

    feat: create a separate component for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import styled from 'styled-components'
-
-const Header = styled.header`
-  border-bottom:  ${({ theme }) => `1px solid ${theme.colors.backgroundGray}`};
-  height: 5rem;
-  display: flex;
-  align-items: center;
-
-  ${({ theme }) => theme.media.tablet} {
-    height: 7rem;
-  }
-
-  ${({ theme }) => theme.media.desktop} {
-    height: 10rem;
-  }
-`
-
-const HeaderText = styled.h1`
-  font-size: 2rem;
-  font-weight: 600;
-  margin: 0;
-  padding: 0 1rem;
-
-  ${({ theme }) => theme.media.tablet} {
-    padding: 0 3rem;
-    font-size: 2.5rem;
-  }
-
-  ${({ theme }) => theme.media.desktop} {
-    padding: 0 5rem;
-    font-size: 3rem;
-  }
-`
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <Header>
-        <HeaderText>
-          Mini CRM
-        </HeaderText>
-      </Header>
+      <Header/>
       <main>
         <Outlet />
       </main>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from 'react-router-dom'
 import App from '../App'
-import Home from '../pages/Home/Home'
+import Home from '../pages/Home'
 
 export const router = createBrowserRouter([
     {
